210926c14ed128a68897278c8fac09a07c3f08ec
Sam Thacker <samuel.thacker.65@my.csun.edu>
2018-04-30 15:39:56 -0700

    Testing function call from page

--- broadcast_dashboard_preview.js
+++ broadcast_dashboard_preview.js
@@ -4,3 +4,8 @@
  * 
  * This file allows for real-time previewing of messages before they're sent out.
  */
+
+ function runPreview() {
+ 	alert('The preview test triggered.');
+ 	console.log("The preview test for log triggered.");
+ }
