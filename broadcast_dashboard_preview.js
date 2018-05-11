150f766f3c56b7e7fca02d8b09fb225aefe754e1
Sam Thacker <samuel.thacker.65@my.csun.edu>
2018-05-11 10:26:05 -0700

    New edit alert push

--- broadcast_dashboard_preview.js
+++ broadcast_dashboard_preview.js
@@ -5,8 +5,6 @@
  * This file allows for real-time previewing of messages before they're sent out.
  */
 
-function runPreview(test) {
- //function runPreview(test) {
- 	alert(test);
- 	alert($("#broadcast_dashboard_custom_message").val());
- }
+$('#edit-preview').click(function () {
+   alert($("#broadcast_dashboard_custom_message").val());
+});
