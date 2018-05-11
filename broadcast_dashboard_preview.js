7f4705aa40a76071f43c214ab57accaf44a1e241
Sam Thacker <samuel.thacker.65@my.csun.edu>
2018-05-11 10:15:23 -0700

    Moved jQuery to runPreview instance

--- broadcast_dashboard_preview.js
+++ broadcast_dashboard_preview.js
@@ -5,14 +5,8 @@
  * This file allows for real-time previewing of messages before they're sent out.
  */
 
- function runPreview(test) {
+jQuery(function runPreview($) {
+ //function runPreview(test) {
  	alert(test);
+ 	alert($("#broadcast_dashboard_custom_message").val());
  }
-(function($){
-
- $("#edit-preview").click(function() {
-    // show the text that is in the textarea element
-    // when the button is clicked.
-    alert($("#broadcast_dashboard_custom_message").val());
-  });
-})(jQuery);
