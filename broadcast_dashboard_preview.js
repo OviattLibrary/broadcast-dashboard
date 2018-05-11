5bf38f5be2edf6539f8a88ae01256e23fed455f3
Sam Thacker <samuel.thacker.65@my.csun.edu>
2018-05-11 10:23:20 -0700

    Move out jQuery to one function

--- broadcast_dashboard_preview.js
+++ broadcast_dashboard_preview.js
@@ -5,7 +5,7 @@
  * This file allows for real-time previewing of messages before they're sent out.
  */
 
-jQuery(function runPreview($)) {
+function runPreview(test) {
  //function runPreview(test) {
  	alert(test);
  	alert($("#broadcast_dashboard_custom_message").val());
