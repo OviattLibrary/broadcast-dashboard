e15fb1889d9fc016618dd7218931437545d8d67f
Sam Thacker <samuel.thacker.65@my.csun.edu>
2018-06-27 11:19:50 -0700

    Not running script until the DOM has loaded

--- broadcast_dashboard_value_grab.js
+++ broadcast_dashboard_value_grab.js
@@ -13,13 +13,14 @@
 	    return selected_option;
 	});
  });*/
+$(document).ready(function() {
+	var input = document.getElementById('edit-broadcast-dashboard-setting-custom-message-edit');
+	var selector = document.getElementById('edit-broadcast-dashboard-setting-custom-message-select');
 
-var input = document.getElementById('edit-broadcast-dashboard-setting-custom-message-edit');
-var selector = document.getElementById('edit-broadcast-dashboard-setting-custom-message-select');
+	console.log('input is: ' + input);
+	console.log('selector is: ' + selector);
 
-console.log('input is: ' + input);
-console.log('selector is: ' + selector);
-
-selector.onchange = function(){
-     input.value = this.value;
+	selector.onchange = function(){
+	     input.value = this.value;
+	}
 }
