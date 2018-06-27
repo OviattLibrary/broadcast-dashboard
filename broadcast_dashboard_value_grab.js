855a192d9ce43490cc1e904dafff9d6ef0faee51
Sam Thacker <samuel.thacker.65@my.csun.edu>
2018-06-27 11:27:01 -0700

    Removed debug for JS, cleaned broken code out

--- broadcast_dashboard_value_grab.js
+++ broadcast_dashboard_value_grab.js
@@ -4,22 +4,10 @@
  * 
  * This file allows for Settings value selection to pass the correct message title to input.
  */
- /*jQuery(function($) {
- 	var selector = document.getElementById("edit-broadcast-dashboard-setting-custom-message-select");
-
- 	selector.addEventListener("change", function() {
-	    //var selected_option = $("#edit-broadcast-dashboard-setting-custom-message-select").val();
-	    var selected_option = selector.val();
-	    return selected_option;
-	});
- });*/
  jQuery(function ($) {
 	$(document).ready(function() {
 		var input = document.getElementById('edit-broadcast-dashboard-setting-custom-message-edit');
 		var selector = document.getElementById('edit-broadcast-dashboard-setting-custom-message-select');
-
-		console.log('input is: ' + input);
-		console.log('selector is: ' + selector);
 
 		selector.onchange = function(){
 		     input.value = this.value;
