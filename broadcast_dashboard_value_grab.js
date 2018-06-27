57635698208a33e005e78ea7690af46d34fac201
Sam Thacker <samuel.thacker.65@my.csun.edu>
2018-06-27 11:11:41 -0700

    Inputting bs_msg_text into value of select fields with bg_msg_title

--- broadcast_dashboard_value_grab.js
+++ broadcast_dashboard_value_grab.js
@@ -2,18 +2,21 @@
  * @file
  * Broadcast Dashboard additional Javascript file
  * 
- * This file allows for Settings value selection to pass the correct message title to SQL.
+ * This file allows for Settings value selection to pass the correct message title to input.
  */
- jQuery(function($) {
+ /*jQuery(function($) {
  	var selector = document.getElementById("edit-broadcast-dashboard-setting-custom-message-select");
 
- 	/*selector.addEventListener("change", function() {
+ 	selector.addEventListener("change", function() {
 	    //var selected_option = $("#edit-broadcast-dashboard-setting-custom-message-select").val();
 	    var selected_option = selector.val();
 	    return selected_option;
-	});*/
+	});
+ });*/
 
-	function getCustomMessage(){
-		return selector.val();
-	}
- }
+var input = document.getElementById('mytext');
+var selector = document.getElementById('dropdown');
+
+selector.onchange = function(){
+     input.value = this.value;
+}
