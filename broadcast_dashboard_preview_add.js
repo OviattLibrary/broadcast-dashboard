a3cdbca4a591eec786b13d64a48926cf5326a69b
Sam Thacker <sammyt523@gmail.com>
2018-07-25 09:56:40 -0700

    Initial refactor commit with similar elements as preview.js

--- broadcast_dashboard_preview_add.js
+++ broadcast_dashboard_preview_add.js
@@ -2,39 +2,44 @@
  * @file
  * Broadcast Dashboard additional Javascript file
  * 
- * This file allows for real-time previewing of messages before they're sent out.
+ * This file allows for real-time previewing of messages before added to the database from the settings page.
  */
 
-var msg_text;
-var custom_clr;
+var custom_clr_class;
+var custom_clr_hex;
+var custom_msg;
 var date;
 
 jQuery(function($) {
 	$('#edit-broadcast-dashboard-settings-add-preview').click(function() {
+		custom_msg = $("#edit-broadcast-dashboard-settings-add-msg-custom-message").val();
 		date = new Date();
 
 		// Set date on click
 		msg_date = "" + date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	  
-	  // Custom
-	  custom_msg = $("#edit-broadcast-dashboard-settings-add-msg-custom-message").val();
-	  custom_clr_class = $("#edit-broadcast-dashboard-settings-add-css-class").val();
-	  custom_clr_hex = $("#edit-broadcast-dashboard-settings-add-hex-code").val();
 
-	  clr_code = color_setter(preset_clr);
-	    
-	  if (clr_code && preset_msg) {
-	    //<div class="'. $clr_code .'" role="alert">' . $preset_msg . ' (Posted on: ' . $msg_date . ')' . $page[$region]['system_main']['#markup'] . '</div>';
-	    $('#markuparea').html('<div class="' + clr_code + '" role="alert">' + preset_msg + " (Posted on: " + msg_date + ')</div>');
-	  }
+    console.log("custom_msg: " + custom_msg);
+    console.log("custom_clr_class: " + custom_clr_class);
+    console.log("custom_clr_hex: " + custom_clr_hex);
 
-	  // For custom alerts
-	  // Set alert color based on setting
-	  clr_code = color_setter(custom_clr);
-	    
-	  if (clr_code && custom_msg) {
-	    //'<div class="'. $clr_code .'" role="alert">' . $cust_msg . ' (Posted on: ' . $msg_date . ')' . $page[$region]['system_main']['#markup'] . '</div>';
-	    $('#markuparea').html('<div class="' + clr_code + '" role="alert">' + custom_msg + " (Posted on: " + msg_date + ')</div>');
-	  }
+    if (current_id != "custom_msg") {
+    	// Not a custom message
+		  if (preset_clr_class != "") {
+		    $('#markuparea').html('<div class="' + preset_clr_class + '" role="alert">' + preset_msg + " (Posted on: " + msg_date + ')</div>');
+		  } else if (preset_clr_hex != "") {
+		    $('#markuparea').html('<div role="alert" style="background-color: ' + preset_clr_hex + ';">' + preset_msg + " (Posted on: " + msg_date + ')</div>');
+		  }
+		  // end no custom msg
+		} else if (current_id == "custom_msg") {
+			console.log("In custom msg markup");
+		  // For custom alerts
+		  if (custom_clr_hex == "") {
+		  	// It's a class
+		    $('#markuparea').html('<div class="' + custom_clr_class + '" role="alert">' + custom_msg + " (Posted on: " + msg_date + ')</div>');
+		  } else if (custom_clr_class == "") {
+		  	// It's a hex
+		    $('#markuparea').html('<div role="alert" style="background-color: ' + custom_clr_hex + ';">' + custom_msg + " (Posted on: " + msg_date + ')</div>');
+		  }
+		} // end custom msg
 	});
 });
