369918aa8459f68993a1184f4f8b906daeb66f1b
Sam Thacker <sammyt523@gmail.com>
2018-08-06 14:09:53 -0700

    Allow editing of name and grabbing values for it

--- broadcast_dashboard_value_grab.js
+++ broadcast_dashboard_value_grab.js
@@ -8,18 +8,29 @@
 	$(document).ready(function() {
 		var selector = document.getElementById('edit-broadcast-dashboard-settings-update-msg-custom-message-select');
 		var id = selector.value;
-		var bd_text_field = document.getElementById('edit-broadcast-dashboard-settings-update-msg-custom-message-edit');
+
+		var bd_msg_field = document.getElementById('edit-broadcast-dashboard-settings-update-msg-custom-message-edit');
 		var hidden_text = document.getElementsByName('broadcast_dashboard_settings_update_msg_msg_text_' + id)[0].value;
 
+		var bd_name_field = document.getElementById('edit-broadcast-dashboard-settings-update-msg-custom-name-edit');
+		var hidden_name = document.getElementsByName('broadcast_dashboard_settings_update_msg_title_' + id)[0].value;
+
 		console.log(id);
+		console.log(hidden_name);
 		console.log(hidden_text);
 
-		bd_text_field.value = hidden_text;
+		bd_name_field.value = hidden_name;
+		bd_msg_field.value = hidden_text;
 
 		selector.onchange = function(){
 			id = selector.value;
+
+			// Change name field text
+			hidden_name = document.getElementsByName('broadcast_dashboard_settings_update_msg_title_' + id)[0].value;
+			bd_name_field.value = hidden_name;
+			// Change msg field text
 			hidden_text = document.getElementsByName('broadcast_dashboard_settings_update_msg_msg_text_' + id)[0].value;
-			bd_text_field.value = hidden_text;
+			bd_msg_field.value = hidden_text;
 		}
 	});
 });
