482a3f6425a36eb78e9d33236efce238dcbbb9b7
Sam Thacker <sammyt523@gmail.com>
2018-08-08 09:21:22 -0700

    Fill out form element with actual value

--- broadcast_dashboard_value_grab.js
+++ broadcast_dashboard_value_grab.js
@@ -31,12 +31,12 @@
 
 		if (hidden_class) {
 			console.log(hidden_class.value);
-			bd_clr_class_field = hidden_class.value;
+			bd_clr_class_field.value = hidden_class.value;
 			bd_clr_class_select.checked = true;
 			bd_clr_hex_select.checked = false;
 		} else if (hidden_hex) {
 			console.log(hidden_hex.value);
-			bd_clr_hex_field = hidden_hex.value;
+			bd_clr_hex_field.value = hidden_hex.value;
 			bd_clr_hex_select.checked = true;
 			bd_clr_class_select.checked = false;
 		}
