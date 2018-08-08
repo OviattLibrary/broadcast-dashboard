89f58796b30280d07ed96616e7674a8d47fcc41c
Sam Thacker <sammyt523@gmail.com>
2018-08-08 10:05:33 -0700

    Clear both color fields on selector change

--- broadcast_dashboard_value_grab.js
+++ broadcast_dashboard_value_grab.js
@@ -73,12 +73,16 @@
 
 			if (hidden_class) {
 				hidden_hex = "";
+				bd_clr_class_field.value = "";
+				bd_clr_hex_field.value = "";
 				console.log(hidden_class.value);
 				bd_clr_class_field.value = hidden_class.value;
 				bd_clr_class_select.checked = true;
 				bd_clr_hex_select.checked = false;
 			} else if (hidden_hex) {
 				hidden_class = "";
+				bd_clr_class_field.value = "";
+				bd_clr_hex_field.value = "";
 				console.log(hidden_hex.value);
 				bd_clr_hex_field.value = hidden_hex.value;
 				bd_clr_hex_select.checked = true;
