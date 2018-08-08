2f61c39f2bbc8584ef46e62e9c4416e37a6ae80c
Sam Thacker <sammyt523@gmail.com>
2018-08-08 08:59:37 -0700

    Correctly grabs value of hidden field that alternates state existence

--- broadcast_dashboard_value_grab.js
+++ broadcast_dashboard_value_grab.js
@@ -17,20 +17,26 @@
 
 		var br_clr_class_select = document.getElementById('edit-broadcast-dashboard-settings-update-color-class');
 		var br_clr_hex_select = document.getElementById('edit-broadcast-dashboard-settings-update-color-hex');
-		var hidden_class = document.getElementsByName('broadcast_dashboard_settings_update_msg_clr_class_' + id)[0].value;
-		var hidden_hex = document.getElementsByName('broadcast_dashboard_settings_update_msg_clr_hex_' + id)[0].value;
+		var bd_clr_class_field = document.getElementById('edit-broadcast-dashboard-settings-update-css-class');
+		var bd_clr_hex_field = document.getElementById('edit-broadcast-dashboard-settings-update-hex-code');
+		var hidden_class = document.getElementsByName('broadcast_dashboard_settings_update_msg_clr_class_' + id)[0];
+		var hidden_hex = document.getElementsByName('broadcast_dashboard_settings_update_msg_clr_hex_' + id)[0];
 
 		console.log(id);
 		console.log(hidden_name);
 		console.log(hidden_text);
+		console.log(hidden_class);
+		console.log(hidden_hex);
 
 		bd_name_field.value = hidden_name;
 		bd_msg_field.value = hidden_text;
 
-		if (hidden_class != "") {
+		if (hidden_class) {
+			bd_clr_class_field = hidden_class.value();
 			bd_clr_class_select.checked = true;
 			bd_clr_hex_select.checked = false;
-		} else if (hidden_hex != "") {
+		} else if (hidden_hex) {
+			bd_clr_hex_field = hidden_hex.value();
 			bd_clr_hex_select.checked = true;
 			bd_clr_class_select.checked = false;
 		}
@@ -49,10 +55,12 @@
 			bd_name_field.value = hidden_name;
 			bd_msg_field.value = hidden_text;
 
-			if (hidden_class != "") {
+			if (hidden_class) {
+				bd_clr_class_field = hidden_class.value();
 				bd_clr_class_select.checked = true;
 				bd_clr_hex_select.checked = false;
-			} else if (hidden_hex != "") {
+			} else if (hidden_hex) {
+				bd_clr_hex_field = hidden_hex.value();
 				bd_clr_hex_select.checked = true;
 				bd_clr_class_select.checked = false;
 			}
